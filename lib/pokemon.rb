11d10ec7db9e42d1de85dab669f3eb8f61db58bd
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-08 17:03:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
   end 
   
   def self.find(id, db)
-    rows = db.execute("SELECT * FROM courses WHERE id = ?", id)
+    rows = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     row = rows.first
     Pokemon.new(id: row[0], name: row[1])
   end 
