3053f920365e26253d462e85f79fa6fd65437632
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-08 16:51:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  
+  @@all = []
+  
+  def initialize(id,name,type,db)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+    
+    @@all << self 
+  end 
+  
+  def self.save(id,name,type,db,database_connection)
+    database_connection.execute("INSERT INTO pokemon (id,name,type,db) VALUES (?,?,?,?)",id,name,type,db)
+  end 
+  
 end
